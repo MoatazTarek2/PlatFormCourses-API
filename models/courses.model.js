9d79133393ab50d0400464b0e88708e4c2250427
Moataz Mohamed <moatazmohamedd01@gmail.com>
2026-03-23 02:10:09 +0200

    Add pre-delete middleware to CoursesSchema for cascading deletions

--- models/courses.model.js
+++ models/courses.model.js
@@ -27,4 +27,12 @@ const CoursesSchema=mongoose.Schema({
     default:0
   }
 })
+CoursesSchema.pre('deleteOne', { document: true, query: true }, async function(next) {
+    const Booking_model = require('./Booking.model')
+    const lessons_model = require('./lessons.model')
+    
+    await Booking_model.deleteMany({ course_id: this._id })
+    await lessons_model.deleteMany({ course_id: this._id })
+    next();
+});
 module.exports=mongoose.model('COURSES',CoursesSchema)
